94d4be29897987d6babf7d57993744f068e5f83a
Kamsnyc <kamel.singh1999@gmail.com>
2023-09-11 15:46:33 -0400

    react redux firebase project (learning)

---
+++ src/Feed.js
@@ -0,0 +1,55 @@
+import React, {useState} from "react";
+import "./Feed.css";
+import CreateIcon from "@mui/icons-material/Create";
+import InputOption from "./InputOption";
+import ImageIcon from "@mui/icons-material/Image";
+import { CalendarViewDay, EventNote, Subscriptions } from "@mui/icons-material";
+import Post from "./Post.js";
+
+function Feed() {
+  const [posts, setPosts]= useState([])
+
+  const sendPost = (e) =>{
+      e.preventDefault();
+
+  }
+
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button onClick={sendPost} type="submit">Send</button>
+          </form>
+        </div>
+
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5f9" />
+
+          <InputOption Icon={Subscriptions} title="Video" color="#E7A33E" />
+
+          <InputOption Icon={EventNote} title="Event" color="#C0CBCD" />
+
+          <InputOption
+            Icon={CalendarViewDay}
+            title="Write Article"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+
+      {/* Posts */}
+      {posts.map( (post) => (
+        <Post />
+      ))}
+
+      <Post name={'Kamel Singh'} description={'This is a test'} message={'WOW this worked'} />
+
+
+    </div>
+  );
+}
+
+export default Feed;
